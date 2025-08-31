a367c6bb463190b5859c4f7bc65551bca4e4d0b4
DaniyalIAhmed <m.daniyal0201@gmail.com>
2025-08-31 15:32:06 +0500

    1.0.0 - Completed App Foundation

--- app/(tabs)/setting.tsx
+++ app/(tabs)/setting.tsx
@@ -1,12 +1,42 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import { createSettingsStyles } from "@/assets/styles/setting.style";
+import DangerZone from "@/components/DangerZone";
+import Preferences from "@/components/Preferences";
+import ProgressStats from "@/components/ProgressStats";
+import useTheme from "@/hooks/useTheme";
+import { Ionicons } from "@expo/vector-icons";
+import { LinearGradient } from "expo-linear-gradient";
+import React from "react";
+import { ScrollView, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-const setting = () => {
+const Settings = () => {
+  const { colors } = useTheme();
+  const styles = createSettingsStyles(colors);
   return (
-    <View>
-      <Text>setting</Text>
-    </View>
-  )
-}
+    <LinearGradient
+      colors={colors.gradients.background}
+      style={styles.container}
+    >
+      <SafeAreaView style={styles.safeArea}>
+        <View style={styles.header}>
+          <View style={styles.titleContainer}>
+            <LinearGradient
+              colors={colors.gradients.primary}
+              style={styles.iconContainer}
+            >
+              <Ionicons name="settings-sharp" size={28} color="#fff" />
+            </LinearGradient>
+            <Text style={styles.title}>Settings</Text>
+          </View>
+        </View>
+        <ScrollView style={styles.scrollView} contentContainerStyle={styles.content} showsVerticalScrollIndicator={false}>
+          <ProgressStats/>
+          <Preferences/>
+          <DangerZone/>
+        </ScrollView>
+      </SafeAreaView>
+    </LinearGradient>
+  );
+};
 
-export default setting
+export default Settings;
